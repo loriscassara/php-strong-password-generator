478267e98fd32e9d203c08aa4b6641748878c645
MrElle <cassaralavoro@gmail.com>
2023-12-19 21:02:31 +0100

    add system for generate password (min=4; max=32)

--- form.php
+++ form.php
@@ -8,7 +8,32 @@
     <!-- import bootstrap v5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark text-white">
+
+    <header>
+
+        <!-- page title -->
+        <h1 class="text-center fw-bold pt-5">Strong Password Generator</h1>
+
+        <!-- page subtitle -->
+        <h3 class="text-center fw-bold pt-3 fst-italic">Genera una password sicura</h3>
+
+    </header>
+
+    <main class="d-flex justify-content-center">
+    
+        <!-- input form password generator -->
+        <div class="input-container text-center" style="width: 50%; margin: 0 auto;">
+		    <form action="password.php" class="pt-5">
+  			    <div class="mb-3">
+    			    <label for="passLenght" class="form-label">Lunghezza Password:</label>
+    			    <input type="number" class="form-control" id="password-lenght" name="passLenght" aria-describedby="emailHelp" placeholder="Password Lenght.." min="4"       max="32" style="width: 35%; margin: 0 auto;">
+  			    </div>
+  			    <button type="submit" class="btn btn-primary">Invia</button>
+		    </form>
+	    </div>
+
+    </main>
     
     <!-- import bootstrap v5.3 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- password.php
+++ password.php
@@ -1,3 +1,34 @@
+<!-- php variables -->
+<?php 
+
+    //Array letters
+    $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    //Array uppercase letters
+    $uppLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    //Array numbers
+    $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    //Array special characters
+    $characters = ['!', '?', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', '@', '#', '|', '<', ',', '>', '.', '/'];
+
+    //Array single
+    $passwordGen = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '!', '?', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', '@', '#', '|', '<', ',', '>', '.', '/'];
+
+    //Password lenght from form.php
+    $passwordLenght = $_GET['passLenght'] ?? "Nessun numero inserito";
+
+    //Password generate
+    $password = '';
+
+    for ($i = 0; $i < $passwordLenght; $i++){
+        $character = $passwordGen[array_rand($passwordGen)];
+        $password .= $character;
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +39,24 @@
     <!-- import bootstrap v5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark text-white">
+
+    <header>
+
+        <!-- page title -->
+        <h1 class="text-center fw-bold pt-5">Password:</h1>
+    
+    </header>
+
+    <main>
+        
+        <div class="password-container text-center">
+            <?php 
+                echo '<p>' . $password . '</p>';
+            ?>
+        </div>
+
+    </main>
     
     <!-- import bootstrap v5.3 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
